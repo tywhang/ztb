410a18fe7cd70bf240a3162513b641dbf0c07ab8
Ted <taeywhang@gmail.com>
2015-10-04 17:38:43 -0700

    Removed users/new

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'main#index'
 
-  get 'sign_up' => 'users#new'
+  post 'users/create' => 'users#create'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
